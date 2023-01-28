e69d271a0b46a858369e1e8822094cbbee41f22b
berger <robertberger.dev@gmail.com>
2023-01-28 14:09:41 -0500

    implimented the extra challenge and tested

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const randomGenerator_1 = require("./randomGenerator");
-// console.log(flipCoin());
 let dieArr = [];
 for (let i = 0; i < 10; i++) {
     dieArr.push((0, randomGenerator_1.rollDice)());
 }
 console.log(dieArr);
 let coinArr = [];
+let headsCount = 0;
+let tailsCount = 0;
 for (let i = 0; i < 50; i++) {
-    coinArr.push((0, randomGenerator_1.flipCoin)());
+    let thisFlip = (0, randomGenerator_1.flipCoin)();
+    coinArr.push(thisFlip);
+    if (thisFlip === 'Heads') {
+        headsCount = headsCount + 1;
+    }
+    else {
+        tailsCount = tailsCount + 1;
+    }
 }
 console.log(coinArr);
+console.log('Number of heads: ' + headsCount);
+console.log('Number of tails: ' + tailsCount);
--- index.ts
+++ index.ts
@@ -8,7 +8,17 @@ console.log(dieArr);
 
 
 let coinArr: Array<string> = [ ];
+let headsCount: number = 0;
+let tailsCount: number = 0;
 for (let i = 0; i < 50; i++) {
-    coinArr.push(flipCoin());
+    let thisFlip = flipCoin();
+    coinArr.push(thisFlip);
+    if (thisFlip === 'Heads') {
+        headsCount = headsCount + 1;
+    } else {
+        tailsCount = tailsCount + 1;
+    }
 }
 console.log(coinArr);
+console.log('Number of heads: ' + headsCount);
+console.log('Number of tails: ' + tailsCount);
